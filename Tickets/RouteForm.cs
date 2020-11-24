ed6ee345ad72e09c0b17ac6bd4a2eac70be4a543
Mikhail Abramov <beavisabra@yandex.ru>
2020-11-25 00:35:23 +0400

    Соединение с БД. Функция для Select-запросов

---
+++ Tickets/RouteForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.Common;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Tickets
+{
+    public partial class RouteForm : Form
+    {
+        public RouteForm()
+        {
+            InitializeComponent();
+
+            int y = 0;
+            System.Collections.Generic.List<string> cities = SQLClass.Select(
+                "SELECT City, TimeStart FROM `routes` WHERE TrainId = 7 ORDER BY TimeStart");
+            
+            for(int i = 0; i < cities.Count;i += 2)
+            {
+                Label lbl = new Label();
+                lbl.Location = new Point(0, y);
+                lbl.Size = new Size(300, 30);
+                string city = 
+                    SQLClass.Select("SELECT Name FROM cities WHERE Id = " + cities[i])[0];
+
+                lbl.Text = city + " " + cities[i+1];
+                Controls.Add(lbl);
+                y += 30;
+            }
+        }
+
+        private void RouteForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Tickets/SQLClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace Tickets
+{
+    public static class SQLClass
+    {
+        public static MySqlConnection conn;
+        public static List<string> Select(string Text)
+        {
+            List<string> results = new List<string>();
+
+            MySqlCommand command = new MySqlCommand(Text, conn);
+            DbDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                    results.Add(reader.GetString(i));
+            }
+
+            reader.Close();
+            command.Dispose();
+            return results;
+        }
+    }
+}
